a4bb6d12bbd122694da0bc66db7e106afdd684a5
iosDevelopForYou <iosdevelopforyou@gmail.com>
2025-01-07 14:50:22 +0400

    Initial Commit

---
+++ AIChatBot/AIChatBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatBotApp.swift
+//  AIChatBot
+//
+//  Created by Marat Guseynov on 07.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct AIChatBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIChatBotTests/AIChatBotTests.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatBotTests.swift
+//  AIChatBotTests
+//
+//  Created by Marat Guseynov on 07.01.2025.
+//
+
+import Testing
+@testable import AIChatBot
+
+struct AIChatBotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
