6267bf3bdd4fd65c4c8069c30ace5afa8b9870dd
iosDevelopForYou <iosdevelopforyou@gmail.com>
2025-01-13 15:29:04 +0400

    [Feature] Add WelcomeView UI

--- AIChatBot/Core/Welcome/WelcomeView.swift
+++ AIChatBot/Core/Welcome/WelcomeView.swift
@@ -8,20 +8,70 @@
 import SwiftUI
 
 struct WelcomeView: View {
+    
+    @State var imageName: String = Constants.randomImage
+    
     var body: some View {
         NavigationStack {
-            VStack {
-                Text("Welcome to AIChatBot!")
-                    .frame(maxHeight: .infinity)
+            VStack(spacing: 10) {
+                ImageLoaderView(urlString: imageName)
+                    .ignoresSafeArea()
                 
-                NavigationLink {
-                    OnboardingCompletedView()
-                } label: {
-                    Text("Get started")
-                        .callToActionButton()
-                }
+                titleSection
+                    .padding(.top, 24)
+                
+                ctaButtons
+                    .padding(16)
+                
+                policyLinks
             }
-            .padding(16)
+        }
+    }
+    
+    private var titleSection: some View {
+        VStack(spacing: 10) {
+            Label("AIChatBot", systemImage: "brain")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+            
+            Text("YouTube @ProIOS")
+                .font(.caption)
+                .foregroundStyle(.secondary)
+          
+        }
+    }
+    
+    private var ctaButtons: some View {
+        VStack(spacing: 10) {
+            NavigationLink {
+                OnboardingCompletedView()
+            } label: {
+                Text("Get started")
+                    .callToActionButton()
+            }
+            
+            Text("Already have an account? Sign in!")
+                .underline()
+                .font(.body)
+                .padding(8)
+                .tappableBackground()
+                .onTapGesture {
+                    
+                }
+        }
+    }
+    
+    private var policyLinks: some View {
+        HStack(spacing: 8) {
+            Link(destination: URL(string: Constants.termsOfServiceURL)!) {
+                Text("Terms of Service")
+            }
+            Circle()
+                .fill(.accent)
+                .frame(width: 4, height: 4)
+            Link(destination: URL(string: Constants.privacyPolicyURL)!) {
+                Text("Privacy Policy")
+            }
         }
     }
 }
---
+++ AIChatBot/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  AIChatBot
+//
+//  Created by Marat Guseynov on 13.01.2025.
+//
+import Foundation
+
+struct Constants {
+    static let randomImage = "https://picsum.photos/600/600"
+    static let privacyPolicyURL = "https://www.apple.com"
+    static let termsOfServiceURL = "https://www.apple.com"
+}
